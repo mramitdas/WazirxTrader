3df81cfa6d5ff519a05fe5d760348731beb93596
Amit <mramitdas99@gmail.com>
2023-01-23 09:58:16 +0530

    Update: error handling & validation

--- src/trade/user.py
+++ src/trade/user.py
@@ -1,19 +1,29 @@
-from wazirx.rest.client import Client
 from datetime import datetime
+
+from wazirx.rest.client import Client
+
+from .exception import MissingAttributeError
 
 
 class User:
     def __init__(self, api_key=None, api_secret=None):
+        if not api_key:
+            raise MissingAttributeError("api_key required")
+        if not api_secret:
+            raise MissingAttributeError("api_secret required")
+
         self.client = Client(api_key=api_key, secret_key=api_secret)
-        self.ct = datetime.now().timestamp()
-        self.ts = int(self.ct * 1000)
 
     def user_info(self):
+        ct = datetime.now().timestamp()
+        ts = int(ct * 1000)
         return self.client.send("account_info",
-                                {"timestamp": self.ts,
+                                {"timestamp": ts,
                                  "recvWindow": 5000})
 
     def user_funds(self):
+        ct = datetime.now().timestamp()
+        ts = int(ct * 1000)
         return self.client.send("funds_info",
-                                {"timestamp": self.ts,
+                                {"timestamp": ts,
                                  "recvWindow": 5000})
