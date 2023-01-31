e33df1628e99034c9517fe6fcce58e342b4dba5d
Amit <mramitdas99@gmail.com>
2023-01-31 10:18:09 +0530

    Feature: added common validator function

--- src/trade/order.py
+++ src/trade/order.py
@@ -15,20 +15,95 @@ class Order:
         self.client = Client(api_key=api_key, secret_key=api_secret)
         self.recvWindow = 2000
 
+    def validate(self, new_order=False,
+                 symbol=None, side=None, order_type=None,
+                 quantity=None, price=None, stop_price=None,
+                 query_order=False, order_id=None,
+                 cancel_order=False,
+                 process_order=False, process_type=None, data=None):
+        if new_order:
+            if not symbol:
+                raise MissingAttributeError("symbol required")
+            else:
+                if not type(symbol) is str:
+                    raise TypeError(
+                        f"Expected a str for 'symbol' but received a {type(symbol).__name__}.")
+
+            if not side:
+                raise MissingAttributeError("side required")
+            else:
+                if not type(symbol) is str:
+                    raise TypeError(
+                        f"Expected a str for 'side' but received a {type(side).__name__}.")
+
+            if not order_type:
+                raise MissingAttributeError("order_type required")
+            else:
+                if not type(symbol) is str:
+                    raise TypeError(
+                        f"Expected a str for 'order_type' but received a {type(order_type).__name__}.")
+
+            if not quantity:
+                raise MissingAttributeError("quantity required")
+            else:
+                if not type(quantity) is float:
+                    raise TypeError(
+                        f"Expected a float for 'quantity' but received a {type(quantity).__name__}.")
+
+            if not price:
+                raise MissingAttributeError("price required")
+            else:
+                if not type(symbol) is float:
+                    raise TypeError(
+                        f"Expected a float for 'price' but received a {type(price).__name__}.")
+
+            if not stop_price:
+                raise MissingAttributeError("stop_price required")
+            else:
+                if not type(stop_price) is float:
+                    raise TypeError(
+                        f"Expected a float for 'stop_price' but received a {type(stop_price).__name__}.")
+
+        elif query_order:
+            if not order_id:
+                raise MissingAttributeError("order_id required")
+            else:
+                if not type(order_id) is str:
+                    raise TypeError(
+                        f"Expected a str for 'order_id' but received a {type(order_id).__name__}.")
+
+        elif cancel_order:
+            if not symbol:
+                raise MissingAttributeError("symbol required")
+            else:
+                if not type(symbol) is str:
+                    raise TypeError(
+                        f"Expected a str for 'symbol' but received a {type(symbol).__name__}.")
+
+            if not order_id:
+                raise MissingAttributeError("order_id required")
+            else:
+                if not type(order_id) is str:
+                    raise TypeError(
+                        f"Expected a str for 'order_id' but received a {type(order_id).__name__}.")
+
+        elif process_order:
+            if not process_type:
+                raise MissingAttributeError("process_type required")
+            else:
+                if not type(process_type) is str:
+                    raise TypeError(
+                        f"Expected a str for 'process_type' but received a {type(process_type).__name__}.")
+
+            if not data:
+                raise MissingAttributeError("data required")
+            else:
+                if not type(data) is dict:
+                    raise TypeError(
+                        f"Expected a dict for 'data' but received a {type(data).__name__}.")
+
     def test_order(self, symbol=None, side=None, order_type=None,
                    quantity=None, price=None, stop_price=None):
-        if not symbol:
-            raise MissingAttributeError("symbol required")
-        if not side:
-            raise MissingAttributeError("side required")
-        if not order_type:
-            raise MissingAttributeError("order_type required")
-        if not quantity:
-            raise MissingAttributeError("quantity required")
-        if not price:
-            raise MissingAttributeError("price required")
-        if not stop_price:
-            raise MissingAttributeError("stop_price required")
 
         ct = datetime.now().timestamp()
         ts = int(ct * 1000)
@@ -44,18 +119,6 @@ class Order:
 
     def new_order(self, symbol=None, side=None, order_type=None,
                   quantity=None, price=None, stop_price=None):
-        if not symbol:
-            raise MissingAttributeError("symbol required")
-        if not side:
-            raise MissingAttributeError("side required")
-        if not order_type:
-            raise MissingAttributeError("order_type required")
-        if not quantity:
-            raise MissingAttributeError("quantity required")
-        if not price:
-            raise MissingAttributeError("price required")
-        if not stop_price:
-            raise MissingAttributeError("stop_price required")
 
         ct = datetime.now().timestamp()
         ts = int(ct * 1000)
@@ -70,8 +133,6 @@ class Order:
                                                  })
 
     def query_order(self, order_id=None):
-        if not order_id:
-            raise MissingAttributeError("order_id required")
 
         ct = datetime.now().timestamp()
         ts = int(ct * 1000)
@@ -80,10 +141,6 @@ class Order:
                                                 "timestamp": ts})
 
     def cancel_order(self, symbol=None, order_id=None):
-        if not symbol:
-            raise MissingAttributeError("symbol required")
-        if not order_id:
-            raise MissingAttributeError("order_id required")
 
         ct = datetime.now().timestamp()
         ts = int(ct * 1000)
@@ -91,3 +148,44 @@ class Order:
                                                  "orderId": order_id,
                                                  "recvWindow": self.recvWindow,
                                                  "timestamp": ts})
+
+    def process_order(self, process_type=None, data=None):
+
+        self.valipassdate(process_order=True)
+
+        if process_type == "test_order":
+            self.validate(new_order=True, symbol=data.get("symbol"),
+                          side=data.get("side"), order_type=data.get("order_type"),
+                          quantity=data.get("quantity"), price=data.get("price"),
+                          stop_price=data.get("stop_price"))
+
+            self.test_order(symbol=data.get("symbol"),
+                            side=data.get("side"), order_type=data.get("order_type"),
+                            quantity=data.get("quantity"), price=data.get("price"),
+                            stop_price=data.get("stop_price"))
+
+        elif process_type == "new_order":
+            self.validate(new_order=True, symbol=data.get("symbol"),
+                          side=data.get("side"), order_type=data.get("order_type"),
+                          quantity=data.get("quantity"), price=data.get("price"),
+                          stop_price=data.get("stop_price"))
+
+            self.new_order(symbol=data.get("symbol"),
+                           side=data.get("side"), order_type=data.get("order_type"),
+                           quantity=data.get("quantity"), price=data.get("price"),
+                           stop_price=data.get("stop_price"))
+
+        elif process_type == "query_order":
+            self.validate(query_order=True, order_id=data.get("order_id"))
+
+            self.query_order(order_id=data.get("order_id"))
+
+        elif process_type == "cancel_order":
+            self.validate(cancel_order=True, symbol=data.get(
+                "symbol"), order_id=data.get("order_id"))
+
+            self.cancel_order(symbol=data.get(
+                "symbol"), order_id=data.get("order_id"))
+
+        else:
+            return {"error": "invalid process type"}
