9bfa041565c1a6053ac4fcc805957eaee6d627db
Amit <mramitdas99@gmail.com>
2023-02-06 20:26:55 +0530

    Update: dynamic quantity & trade_limit allocation

--- src/trade/quantity.py
+++ src/trade/quantity.py
@@ -2,7 +2,7 @@ from .exception import MissingAttributeError
 
 
 class Quantity:
-    def __init__(self, base_amount=None, asset={}, share_percent=30):
+    def __init__(self, base_amount=None, asset={}, share_percent=33):
         if not base_amount:
             raise MissingAttributeError("base_amount required")
         if asset == {}:
@@ -19,5 +19,13 @@ class Quantity:
         funds_issued = self.amount_calculator()
 
         for symbol in self.asset:
-            self.asset[symbol]["quantity"] = round(
+            symbol_quantity = round(
                 ((funds_issued / self.asset[symbol]["buy"]) * (self.share_percent / 100)), 1)
+
+            if (symbol_quantity * self.asset[symbol]["buy"]) > 60:
+                self.asset[symbol]["quantity"] = symbol_quantity
+            else:
+                self.asset[symbol]["quantity"] = 60 / self.asset[symbol]["buy"]
+
+            self.asset[symbol]["trade_limit"] = int(funds_issued /
+                                                    (symbol_quantity * self.asset[symbol]["buy"]))
