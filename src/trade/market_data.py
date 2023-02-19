5b11a0da4de6268212a5d556df6f57ffcf5798e9
Amit <mramitdas99@gmail.com>
2023-02-20 01:05:34 +0530

    DOCS & REFACTOR: added func docstring & autopep8 changes

--- src/trade/market_data.py
+++ src/trade/market_data.py
@@ -8,6 +8,16 @@ from .exception import MissingAttributeError
 
 class MarketData:
     def __init__(self, api_key=None, api_secret=None):
+        """_summary_
+
+        Args:
+            api_key (str): wazirx api key
+            api_secret (str): wazirx secret key
+        Returns:
+            self.client: obj instance
+            self.filtered_asset (dict): symbols qualified for trading
+        """
+
         if not api_key:
             raise MissingAttributeError("api_key required")
         if not api_secret:
@@ -16,80 +26,159 @@ class MarketData:
         self.client = Client(api_key=api_key, secret_key=api_secret)
         self.filtered_asset = {}
 
-    def validate(self,
-                 get_symbol_depth=None, symbol=None, limit=None,
-                 process_symbol=None, apply_filter=None, symbols=None, amount_limit=None, symbol_limit=None, depth_limit=None):
+    def validate(
+        self,
+        get_symbol_depth=None,
+        symbol=None,
+        limit=None,
+        process_symbol=None,
+        apply_filter=None,
+        symbols=None,
+        amount_limit=None,
+        symbol_limit=None,
+        depth_limit=None,
+    ):
+        """_summary_
 
+        Acts as middleware (serializer)
+        accepts multiple arguments and raise MissingAttributeError & TypeError if missing/invalid
+        """
         if process_symbol:
             if symbols:
-                if not type(symbols) is list:
+                if not isinstance(symbols, list):
                     raise TypeError(
-                        f"Expected a list for 'symbols' but received a {type(symbols).__name__}.")
+                        f"Expected a list for 'symbols' but received a {type(symbols).__name__}."
+                    )
             if apply_filter:
                 if not amount_limit:
                     raise MissingAttributeError("amount_limit required")
                 else:
-                    if not type(amount_limit) is int:
+                    if not isinstance(amount_limit, int):
                         raise TypeError(
-                            f"Expected a int for 'amount_limit' but received a {type(amount_limit).__name__}.")
+                            f"Expected a int for 'amount_limit' but received a {type(amount_limit).__name__}."
+                        )
 
                 if not symbol_limit:
                     raise MissingAttributeError("symbol_limit required")
                 else:
-                    if not type(symbol_limit) is int:
+                    if not isinstance(symbol_limit, int):
                         raise TypeError(
-                            f"Expected a int for 'symbol_limit' but received a {type(symbol_limit).__name__}.")
+                            f"Expected a int for 'symbol_limit' but received a {type(symbol_limit).__name__}."
+                        )
 
                 if not depth_limit:
                     raise MissingAttributeError("depth_limit required")
                 else:
-                    if not type(depth_limit) is int:
+                    if not isinstance(depth_limit, int):
                         raise TypeError(
-                            f"Expected a int for 'depth_limit' but received a {type(depth_limit).__name__}.")
+                            f"Expected a int for 'depth_limit' but received a {type(depth_limit).__name__}."
+                        )
 
         else:
             if not symbol:
                 raise MissingAttributeError("symbol required")
             else:
-                if not type(symbol) is str:
+                if not isinstance(symbol, str):
                     raise TypeError(
-                        f"Expected a str for 'symbol' but received a {type(symbol).__name__}.")
+                        f"Expected a str for 'symbol' but received a {type(symbol).__name__}."
+                    )
 
             if get_symbol_depth:
                 if not limit:
                     raise MissingAttributeError("limit required")
                 else:
-                    if not type(limit) is int:
+                    if not isinstance(limit, int):
                         raise TypeError(
-                            f"Expected a int for 'limit' but received a {type(limit).__name__}.")
-
-    def chart_24hr(self):
-        return self.client.send("tickers")
+                            f"Expected a int for 'limit' but received a {type(limit).__name__}."
+                        )
 
     def symbol_chart_24hr(self, symbol=None):
-        self.validate(symbol=symbol)
+        """_summary_
 
-        return self.client.send("ticker", {"symbol": symbol})
+        24 hour rolling window price change statistics.
 
-    def trades(self, symbol, trade_limit):
-        self.validate(symbol=symbol, )
+        Rate limit: 1 per second
+        Query Parameters:
+            symbol: crypto name
+        Returns:
+            dict: {}
+        """
+        if symbol:
+            self.validate(symbol=symbol)
+            return self.client.send("ticker", {"symbol": symbol})
+        else:
+            return self.client.send("tickers")
 
-        return self.client.send("trades", {"symbol": symbol,
-                                           "limit": trade_limit})
+    def trades(self, symbol, limit):
+        """_summary_
+
+        Get recent trades.
+
+        Rate limit: 1 per second
+        Query Parameters:
+            symbol: crypto name
+            limit: no of records
+        Returns:
+            dict: {}
+        """
+        self.validate(symbol=symbol, get_symbol_depth=True, limit=limit)
+        return self.client.send("trades", {"symbol": symbol, "limit": limit})
 
     def get_symbol_depth(self, symbol=None, limit=None):
-        self.validate(symbol=symbol, get_symbol_depth=True, limit=limit)
+        """_summary_
 
-        return self.client.send("depth", {"symbol": symbol,
-                                          "limit": limit})
+        Get symbol depth.
+
+        Rate limit: 1 per second
+        Query Parameters:
+            symbol: crypto name
+            limit: no of records
+        Returns:
+            dict: {}
+        """
+        self.validate(symbol=symbol, get_symbol_depth=True, limit=limit)
+        return self.client.send("depth", {"symbol": symbol, "limit": limit})
 
     def symbol_depth_calculator(self, buy, sell):
+        """_summary_
+
+        Args:
+            buy (float): asset buy price
+            sell (float): asset sell price
+
+        Returns:
+            float: percentage at which profit can be booked
+        """
         return ((sell - buy) / buy) * 100
 
-    def process_symbol(self, symbols=None, apply_filter=None, amount_limit=None, symbol_limit=None, depth_limit=None):
+    def process_symbol(
+        self,
+        symbols=None,
+        apply_filter=None,
+        amount_limit=None,
+        symbol_limit=None,
+        depth_limit=None,
+    ):
+        """_summary_
+        The function will process and filter symbols, either those provided by the user or those found in a symbol file.
+        The output will consist of the best symbols that were identified.
 
-        self.validate(process_symbol=True, symbols=symbols, apply_filter=apply_filter, amount_limit=amount_limit,
-                      symbol_limit=symbol_limit, depth_limit=depth_limit)
+        Args:
+            symbols (list, optional): cryptocurrencies that the user would like to trade
+            apply_filter (bool, optional): If True, below filters will be applied
+            amount_limit (int, optional): filters out cryptocurrencies based on their amount
+            symbol_limit (int, optional): filters out the top performing cryptocurrencies
+            depth_limit (int, optional): filters out cryptocurrencies based on their market depth
+        """
+
+        self.validate(
+            process_symbol=True,
+            symbols=symbols,
+            apply_filter=apply_filter,
+            amount_limit=amount_limit,
+            symbol_limit=symbol_limit,
+            depth_limit=depth_limit,
+        )
 
         if symbols is not None:
             symbol_list = symbols
@@ -107,24 +196,36 @@ class MarketData:
                 depth_percentage = self.symbol_depth_calculator(buy, sell)
                 if apply_filter:
                     if depth_percentage >= depth_limit:
-                        self.filtered_asset[symbol] = {"depth": depth_percentage,
-                                                       "buy": buy,
-                                                       "sell": sell}
+                        self.filtered_asset[symbol] = {
+                            "depth": depth_percentage,
+                            "buy": buy,
+                            "sell": sell,
+                        }
                 else:
-                    self.filtered_asset[symbol] = {"depth": depth_percentage,
-                                                   "buy": buy,
-                                                   "sell": sell}
+                    self.filtered_asset[symbol] = {
+                        "depth": depth_percentage,
+                        "buy": buy,
+                        "sell": sell,
+                    }
             except Exception:
                 pass
 
-        # print(self.filtered_asset)
         if apply_filter:
-            self.filtered_asset = dict(sorted(self.filtered_asset.items(),
-                                              key=lambda x: x[1]['depth'], reverse=True))
+            self.filtered_asset = dict(
+                sorted(
+                    self.filtered_asset.items(),
+                    key=lambda x: x[1]["depth"],
+                    reverse=True,
+                )
+            )
 
-            self.filtered_asset = dict(filter(lambda item:
-                                              item[1]["buy"] < amount_limit,
-                                              self.filtered_asset.items()))
+            self.filtered_asset = dict(
+                filter(
+                    lambda item: item[1]["buy"] < amount_limit,
+                    self.filtered_asset.items(),
+                )
+            )
 
-            self.filtered_asset = dict(itertools.islice(
-                self.filtered_asset.items(), symbol_limit))
+            self.filtered_asset = dict(
+                itertools.islice(self.filtered_asset.items(), symbol_limit)
+            )
