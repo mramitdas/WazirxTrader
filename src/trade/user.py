ef1cee5a2f7e0e50f2b43de294f2594be7ec655b
Amit <mramitdas99@gmail.com>
2023-02-20 10:13:18 +0530

    DOCS: added user related function docstring

--- src/trade/user.py
+++ src/trade/user.py
@@ -7,6 +7,14 @@ from .exception import MissingAttributeError
 
 class User:
     def __init__(self, api_key=None, api_secret=None):
+        """_summary_
+
+        Args:
+            api_key (str): wazirx api key
+            api_secret (str): wazirx secret key
+        Returns:
+            self.client: obj instance
+        """
         if not api_key:
             raise MissingAttributeError("api_key required")
         if not api_secret:
@@ -15,6 +23,15 @@ class User:
         self.client = Client(api_key=api_key, secret_key=api_secret)
 
     def user_info(self):
+        """_summary_
+
+        Get current account information.
+
+        Rate limit: 1 per second
+        Query Parameters: None
+        Returns:
+            dict: {}
+        """
         ct = datetime.now().timestamp()
         ts = int(ct * 1000)
         return self.client.send("account_info",
@@ -22,6 +39,15 @@ class User:
                                  "recvWindow": 5000})
 
     def user_funds(self):
+        """_summary_
+
+        Get fund details for current account.
+
+        Rate limit: 1 per second
+        Query Parameters: None
+        Returns:
+            list: [{}, {}]
+        """
         ct = datetime.now().timestamp()
         ts = int(ct * 1000)
         return self.client.send("funds_info",
