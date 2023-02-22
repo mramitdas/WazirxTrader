9e183e058aa014540473462836b198ee00376e82
Amit <mramitdas99@gmail.com>
2023-02-22 23:20:19 +0530

    REFACTOR & DOCS: database -> documentation & autopep8 changes

--- src/trade/database.py
+++ src/trade/database.py
@@ -7,104 +7,147 @@ from .exception import MissingAttributeError
 
 
 class DataBase:
-    """
-    This is a common database class which is called in every function
-
-    Working: Database instance can be created by calling DataBase class along with the required arguments.
-
-    Attributes:
-        db_url -- Database url
-        db_name -- Database name
-        table_name -- Database table name
-
-    Returns:
-        database -- refers to specific database which might contain several tables.
-        dataset -- refers to specific table present in database.
-        storage -- refers to storage container which contains data like image, pdf,..,etc.
-    """
-
     def __init__(self, db_url=None):
+        """_summary_
+        initiate mongodb instance
+
+        Args:
+            db_url (str): database connection url
+        Response:
+            self.mongod: mongodb instance
+        """
         if not db_url:
             raise MissingAttributeError("db_url required")
         else:
-            if not type(db_url) is str:
+            if not isinstance(db_url, str):
                 raise TypeError(
-                    f"Expected a str for 'db_url' but received a {type(db_url).__name__}.")
+                    f"Expected a str for 'db_url' but received a {type(db_url).__name__}."
+                )
 
         self.database_url = db_url
         self.mongod = self.connect()
 
     def connect(self):
+        """_summary_
+        acts as middleware while connecting to mongo instance
+
+        Returns:
+            obj: mongo client
+        """
         return pymongo.MongoClient(self.database_url)
 
-    def validate(self, db_name=None, table_name=None,
-                 data_opt=False, data=None,
-                 filter_opt=False, filter=None,
-                 bulk=False):
+    def validate(
+        self,
+        db_name=None,
+        table_name=None,
+        data_opt=False,
+        data=None,
+        filter_opt=False,
+        filter=None,
+        bulk=False,
+    ):
+        """_summary_
+
+        Acts as middleware (serializer)
+        accepts multiple arguments and raise MissingAttributeError & TypeError if missing/invalid
+        """
         if not db_name:
             raise MissingAttributeError("db_name required")
         else:
-            if not type(db_name) is str:
+            if not isinstance(db_name, str):
                 raise TypeError(
-                    f"Expected a str for 'db_name' but received a {type(db_name).__name__}.")
+                    f"Expected a str for 'db_name' but received a {type(db_name).__name__}."
+                )
 
         if not table_name:
             raise MissingAttributeError("table_name required")
         else:
-            if not type(table_name) is str:
+            if not isinstance(table_name, str):
                 raise TypeError(
-                    f"Expected a str for 'table_name' but received a {type(table_name).__name__}.")
+                    f"Expected a str for 'table_name' but received a {type(table_name).__name__}."
+                )
 
         if data_opt:
             if not data:
                 raise MissingAttributeError("data required")
             else:
-                if not (type(data) is dict or type(data) is list):
+                if not (isinstance(data, dict) or isinstance(data, list)):
                     raise TypeError(
-                        f"Expected a dict/list for 'data' but received a {type(data).__name__}.")
+                        f"Expected a dict/list for 'data' but received a {type(data).__name__}."
+                    )
 
         if filter_opt:
             if not filter:
                 raise MissingAttributeError("filter required")
             else:
-                if not type(filter) is dict:
+                if not isinstance(filter, dict):
                     raise TypeError(
-                        f"Expected a dict for 'filter' but received a {type(filter).__name__}.")
+                        f"Expected a dict for 'filter' but received a {type(filter).__name__}."
+                    )
 
         if bulk:
-            if not type(bulk) is bool:
+            if not isinstance(bulk, bool):
                 raise TypeError(
-                    f"Expected a bool for 'bulk' but received a {type(bulk).__name__}.")
+                    f"Expected a bool for 'bulk' but received a {type(bulk).__name__}."
+                )
 
     def upload(self, db_name=None, table_name=None, data=None):
+        """_summary_
+
+        Args:
+            db_name (str): database name.
+            table_name (str): table name.
+            data (dict, list): data to be dumped over database.
+                dict -> to be used for single insertion.
+                list -> to be used for bulk insertion.
+
+        Returns:
+            response: response code, data
+        """
         self.validate(db_name, table_name, data_opt=True, data=data)
 
         # time-zone
-        ist = pytz.timezone('Asia/Kolkata')
+        ist = pytz.timezone("Asia/Kolkata")
 
         database = self.mongod[db_name]
         dataset = database[table_name]
 
-        if type(data) is dict:
-            time_stamp = datetime.now(ist).strftime('%Y-%m-%d || %H:%M:%S:%f')
+        if isinstance(data, dict):
+            time_stamp = datetime.now(ist).strftime("%Y-%m-%d || %H:%M:%S:%f")
             data["_id"] = time_stamp
             response = dataset.insert_one(data)
         else:
-            [x.update(
-                {"_id": datetime.now(ist).strftime('%Y-%m-%d || %H:%M:%S:%f')}
-            ) for x in data]
+            [
+                x.update({"_id": datetime.now(ist).strftime(
+                    "%Y-%m-%d || %H:%M:%S:%f")})
+                for x in data
+            ]
             response = dataset.insert_many(data)
 
         return response
 
     def query(self, db_name=None, table_name=None, filter=None, bulk=False):
+        """_summary_
+
+        Args:
+            db_name (str): database name.
+            table_name (str): table name.
+            filter (dict): filter to be applied over search query
+            bulk (bool): multiple results will be returned, if True
+
+        Returns:
+            response: response code, data
+        """
+        filter_opt = False
         if filter:
             filter_opt = True
-        else:
-            filter_opt = False
 
-        self.validate(db_name, table_name,
-                      filter_opt=filter_opt, filter=filter, bulk=bulk)
+        self.validate(
+            db_name,
+            table_name,
+            filter_opt=filter_opt,
+            filter=filter,
+            bulk=bulk)
 
         database = self.mongod[db_name]
         dataset = database[table_name]
@@ -122,9 +165,19 @@ class DataBase:
         return response
 
     def update(self, db_name=None, table_name=None, data=None, bulk=False):
-        self.validate(db_name, table_name,
-                      data_opt=True, data=data,
-                      bulk=bulk)
+        """_summary_
+
+        Args:
+            db_name (str): database name.
+            table_name (str): table name.
+            data (dict): data to be updated
+            bulk (bool): multiple results will be updated, if True
+
+
+        Returns:
+            response: response code, data
+        """
+        self.validate(db_name, table_name, data_opt=True, data=data, bulk=bulk)
 
         database = self.mongod[db_name]
         dataset = database[table_name]
@@ -137,6 +190,16 @@ class DataBase:
         return response
 
     def delete(self, db_name=None, table_name=None, filter=None):
+        """_summary_
+
+        Args:
+            db_name (str): database name.
+            table_name (str): table name.
+            filter (dict): filter to be applied over search query
+
+        Returns:
+            response: response code, data
+        """
         self.validate(db_name, table_name, filter_opt=True, filter=filter)
 
         database = self.mongod[db_name]
